be25ba228b3affa680293f208c210692f4fe9a26
pooja18328 <poojadattatreya351@gmail.com>
2025-07-11 09:52:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+Javascript file
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Simple JS Example</title>
+</head>
+<body>
+
+<h2>Click the button to see a message:</h2>
+<button onclick="sayHello()">Click Me</button>
+
+<script>
+  function sayHello() {
+    alert("Hello! Have a great day");
+  }
+</script>
+
+</body>
+</html>
